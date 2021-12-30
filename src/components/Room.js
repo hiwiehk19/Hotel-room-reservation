66afff062cad18073d32487f7be91c7e70ee13df
hiwot <hiwotzelalem3@gmal.com>
2021-12-30 12:16:23 +0300

    roon

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import { Link } from "react-router-dom";
 import defaultImg from "../images/room-1.jpeg";
@@ -7,7 +8,10 @@ export default function Room({name, slug, image, price}) {
     <article className="room">
       <div className="img-container">
         <img src={image} alt="single room" />
+        <div className="price-top">
+            <h6>{price}ETB</h6>
+        </div>
       </div>
     </article>
   );
-}
+  }
