482a8618f048ed2986b1cfbc87c1a45258fd34b5
hiwot <hiwotzelalem3@gmal.com>
2022-01-02 19:59:39 +0300

    single room fixed

--- src/pages/SingleRooms.js
+++ src/pages/SingleRooms.js
@@ -1,54 +1,56 @@
-import React from "react";
+import React,{ useContext } from "react";
 import defaultBcg from '../images/room-1.jpeg'
 import Hero from '../components/Hero'
 import Banner from '../components/Banner'
-import {Link} from 'react-router-dom'
+import {Link, useParams} from 'react-router-dom'
 import { RoomContext } from "../context";
 import { Component } from "react/cjs/react.production.min";
-class SingleRooms extends Component  {
-  constructor(props){
-    super(props);
-    this.state = {
-      slug: this.props.match.params.slug,
-      defaultBcg: defaultBcg
-    };
 
-    
-  }
-  static contextType= RoomContext;
 
-render()
-{
-  const { getRoom }= this.context;
-  const room= getRoom(this.state.slug);
-  if(!room){
-    return <div className="error">
-      <h3>no such room could be found...</h3>
-       <Link to ="/rooms" className="btn-primary">
-         back to rooms
-       </Link>
-    </div>
-  }
+const SingleRooms = () =>  {
 
-const {
-  name,
-  description,
-  capacity,
-  size,
-  price,
-  extras,
-  breakfast,
-  pets,
-  images
-} = room;
-return <Hero hero='roomsHero'>
-  <Banner title={`${name} room`}>
-  <Link to ="/rooms" className="btn-primary">
-         back to rooms
-       </Link> 
-  </Banner>
-  </Hero>}
-}; 
+      let { slug } = useParams()
+      //let defaultBcg = defaultBcg
+  
+  const roomContext= useContext(RoomContext);
+
+          const { getRoom }= roomContext;
+          const room= getRoom(slug);
+          console.log(slug)
+          if(!room){
+            return
+            ( 
+                <div className="error">
+                <h3>no such room could be found...</h3>
+                <Link to ="/rooms" className="btn-primary">
+                  back to rooms
+                </Link>
+              </div>
+            )
+          }
+
+        const {
+          name,
+          description,
+          capacity,
+          size,
+          price,
+          extras,
+          breakfast,
+          pets,
+          images
+        } = room;
+      
+        return(
+          <Hero hero='roomsHero'>
+              <Banner title={`${name} room`}>
+              <Link to ="/rooms" className="btn-primary">
+                    back to rooms
+                  </Link> 
+              </Banner>
+              </Hero>
+        )        
+}
 
 export default SingleRooms;
     
